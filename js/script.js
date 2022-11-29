d63fe7cbaed18a4d48ad295cfdac74f50e69cf7f
Yannis <yfourel26600@gmail.com>
2022-11-29 09:38:31 +0100

    Deuxieme commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Animation text with Typed.js*/
+var typed = new Typed(".input__text",{
+    strings:["Etudiant.","Développeur."],
+    typeSpeed: 50,
+    backSpeed: 70,
+    loop: true,
+});
+/*End animation text with Typed.js*/
